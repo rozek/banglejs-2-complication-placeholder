3da6a7d16fe88d2051f086f5f3b8d0884618e9e9
Andreas Rozek <A.Rozek@gmx.de>
2022-01-11 09:42:08 +0100

    Update Complication.js

--- Complication.js
+++ Complication.js
@@ -1,14 +1,27 @@
 (function () {
   require('https://raw.githubusercontent.com/rozek/banglejs-2-drawroundrect/main/drawRoundRect.js');
 
-  exports.draw = function draw (x,y, Radius, Settings) {
+  function drawWithLabel (Label, x,y, Radius, Settings) {
     let halfScreenWidth = g.getWidth() / 2;
+
+    g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
+    g.setFont('Vector', 18);
+    g.setFontAlign(0,0);
+
     if (x === halfScreenWidth) {
-      g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
       g.drawRoundRect(x-3*Radius,y-Radius, x+3*Radius,y+Radius, Radius);
     } else {
-      g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
       g.drawCircle(x,y, Radius);
     }
+
+    if (Label != null) {
+      g.drawString(Label, x+2,y); // as font alignment does not work properly...
+    }
+  }
+
+  exports.draw = drawWithLabel.bind(this,null);
+
+  exports.withLabel = function withLabel (Label) {
+    return { draw:drawWithLabel.bind(this,Label) };
   };
 })();
